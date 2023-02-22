99e0e234e75bd5f507c98264eb3a6ad6d3a5fc45
backjello <jackbello97@gmail.com>
2023-02-22 12:58:25 +0100

    compitofix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   
- 
+ constructor(){
+  const a = {id:3,prezzo:5}
+  const b = {id:3,sconto:10}
+  const c = {...a,...b}
+  console.log(c);
+  
+ }
+
+
 }
